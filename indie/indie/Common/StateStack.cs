bc24df5362f2eed151116afc89ebcb252ec419e3
Markus Nilsson <71263532+markusnilssn@users.noreply.github.com>
2023-05-20 12:42:01 +0200

    add simple statestack

---
+++ indie/indie/Common/StateStack.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndieGame.Common
+{
+    public class State
+    {
+        public State() { }
+        public void Update(GameTime aGameTime) { }
+        public void Render(SpriteBatch aRenderer) { }
+    }
+
+    public class StateStack
+    {
+        private Stack<State> m_Stack;
+
+        public StateStack() 
+        {
+            m_Stack = new Stack<State>();
+        }
+
+        public void Update(GameTime aGameTime)
+        {
+            var state = m_Stack.Peek();
+
+            state.Update(aGameTime);
+        }
+
+        public void Render(SpriteBatch aRenderer)
+        {
+            var state = m_Stack.Peek();
+
+            state.Render(aRenderer);
+        }
+
+        public void Push(State aState)
+        {
+            m_Stack.Push(aState);
+        }
+
+        public void Pop()
+        {
+            m_Stack.Pop();
+        }
+
+        public void Clear()
+        {
+            m_Stack.Clear();
+        }
+
+
+    }
+}
