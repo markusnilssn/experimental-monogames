104ad5e52b60cae125bca19d6730d084622dfe86
Markus Nilsson <71263532+markusnilssn@users.noreply.github.com>
2023-05-20 12:41:54 +0200

    add maff

---
+++ indie/indie/Common/Maff.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndieGame.Common
+{
+    public static class Maff
+    {
+        public static float AngleFromVector(Vector2 vector)
+        {
+            return (float)Math.Atan2(vector.Y, vector.X);
+        }
+
+        public static Vector2 VectorFromLengthAndAngle(float length, float angle)
+        {
+            return new Vector2(length * (float)Math.Cos(angle), length * (float)Math.Sin(angle));
+        }
+
+        public static Color MultiplyAlpha(Color color)
+        {
+            return new Color((color.A * color.R) / 255,
+                (color.A * color.G) / 255,
+                (color.A * color.B) / 255,
+                color.A);
+        }
+    }
+}
